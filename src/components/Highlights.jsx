7def894278cd673fb2c3e53d23d599616db635b3
khursheedahmedd <118465990+khursheedahmedd@users.noreply.github.com>
2024-03-26 02:21:52 +0500

    added highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,42 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import gsap from 'gsap/gsap-core'
+import { rightImg, watchImg } from '../utils'
+import VideoCarosel from './VideoCarousel'
 
 const Highlights = () => {
-    return (
-        <section className='w-screen overflow-hidden h-full common-padding bg-zinc'>
 
+    useGSAP(() => {
+        gsap.to('#title', {
+            opacity: 1,
+            y: 0
+        })
+        gsap.to('.link', {
+            opacity: 1,
+            duration: 1,
+            stagger: 0.25,
+            y: 0
+        })
+    }, [])
+
+    return (
+        <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+            <div className='screen-max-width'>
+                <div className='mb-12 w-full md:flex items-end justify-between'>
+                    <h1 id='title' className='section-heading'>Get the Hightlights.</h1>
+                    <div className='flex flex-wrap items-end gap-5'>
+                        <p className='link'>
+                            Watch the Film
+                            <img src={watchImg} alt="watch" className='ml-2' />
+                        </p>
+                        <p className='link'>
+                            Watch the Event
+                            <img src={rightImg} alt="right" className='ml-2' />
+                        </p>
+                    </div>
+                </div>
+                <VideoCarosel />
+            </div>
         </section>
     )
 }
